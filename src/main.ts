21d4d089876133356e8300121223ee0eda8d6d46
Daksh Taneja <daksh@plutus.gg>
2025-07-12 16:36:17 +0530

    add cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || origin === 'http://localhost:3000') {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 3000);
+  console.log(`Server is running on port ${process.env.PORT}`);
 }
 bootstrap();
