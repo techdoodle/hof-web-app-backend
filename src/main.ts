51a19b5fc5777d86be4462f96d9e783048ec585f
techdoodle <p.gambhir09@gmail.com>
2025-07-29 01:16:25 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     'http://localhost:3000', // Local frontend
     'http://localhost:3001', // Local frontend alternative port
     'https://hof-ui-git-main-techdoodle-3947s-projects.vercel.app',
-    'https://hof-ui.netlify.app/',
+    'https://hof-ui.netlify.app',
     'hof-ui.netlify.com',
     // process.env.FRONTEND_URL, // Production frontend URL
   ].filter(Boolean); // Remove undefined values
