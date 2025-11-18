ac1fc9b1fa08d3e3be9b187c5dcfb7f29f5838cf
control-alt-defeat-31 <daksh.taneja.hof@gmail.com>
2025-11-18 21:34:27 +0530

    expanding throttle limit

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -57,7 +57,7 @@ import { WaitlistModule } from '../waitlist/waitlist.module';
       throttlers: [
         {
           ttl: 60000,
-          limit: 50,
+          limit: 200,
         },
       ],
     }),
